1a423f1f0715ecde0790885b13bec5a20de5f1c6
Vasil Dimov <vd@FreeBSD.org>
2006-03-27 13:21:55 +0000

    add -b option

--- src/portsearch.c
+++ src/portsearch.c
@@ -24,7 +24,6 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
 #include <sys/param.h>
 
 #include <err.h>
@@ -47,7 +46,7 @@
 #define OPT_KEY		"key="
 #define OPT_KEY_LEN	4
 
-__RCSID("$Id: portsearch.c,v 1.17 2006/03/27 12:36:55 dd Exp $");
+__RCSID("$Id: portsearch.c,v 1.18 2006/03/27 13:21:55 dd Exp $");
 
 /*
  * Retrieve PORTSDIR using make -V PORTSDIR
@@ -151,21 +150,22 @@ usage()
 	fprintf(stderr, "update/create database:\n");
 	fprintf(stderr, "  %s -u [-H portshome] [-vvv]\n", prog);
 	fprintf(stderr, "\n");
-	fprintf(stderr, "search for ports (based on extended regular expressions, case sensitive):\n");
-	fprintf(stderr, "  -n name\tby name (%s can be used)\n", OPT_NAME);
-	fprintf(stderr, "  -k key\tby name, comment or dependencies (%s can be used)\n", OPT_KEY);
-	fprintf(stderr, "  -p path\tby path on the filesystem\n");
-	fprintf(stderr, "  -i info\tby info (comment)\n");
-	fprintf(stderr, "  -m maint\tby maintainer\n");
-	fprintf(stderr, "  -c cat\tby category\n");
-	fprintf(stderr, "  -F fdep\tby fetch dependencies\n");
-	fprintf(stderr, "  -E edep\tby extract dependencies\n");
-	fprintf(stderr, "  -P pdep\tby patch dependencies\n");
-	fprintf(stderr, "  -B bdep\tby build dependencies\n");
-	fprintf(stderr, "  -R rdep\tby run dependencies\n");
-	fprintf(stderr, "  -D dep\tby build or run dependencies\n");
-	fprintf(stderr, "  -w www\tby www site\n");
-	fprintf(stderr, "  -f file\tthat install file\n");
+	fprintf(stderr, "search for ports (based on extended regular expressions, case sensitive) by:\n");
+	fprintf(stderr, "  -n name\tname (%s can be used)\n", OPT_NAME);
+	fprintf(stderr, "  -k key\tname, comment or dependencies (%s can be used)\n", OPT_KEY);
+	fprintf(stderr, "  -p path\tpath on the filesystem\n");
+	fprintf(stderr, "  -i info\tinfo (comment)\n");
+	fprintf(stderr, "  -m maint\tmaintainer\n");
+	fprintf(stderr, "  -c cat\tcategory\n");
+	fprintf(stderr, "  -F fdep\tfetch dependencies\n");
+	fprintf(stderr, "  -E edep\textract dependencies\n");
+	fprintf(stderr, "  -P pdep\tpatch dependencies\n");
+	fprintf(stderr, "  -B bdep\tbuild dependencies\n");
+	fprintf(stderr, "  -R rdep\trun dependencies\n");
+	fprintf(stderr, "  -D dep\tbuild or run dependencies\n");
+	fprintf(stderr, "  -w www\twww site\n");
+	fprintf(stderr, "  -f file\tpacking list file\n");
+	fprintf(stderr, "  -b file\tpacking list file's basename - same as -f '/file$'\n");
 	fprintf(stderr, "  -I\t\tignore case\n");
 	fprintf(stderr, "  -o fields\toutput fields, default: %s\n", DFLT_OUTFLDS);
 	fprintf(stderr, "\n");
@@ -182,7 +182,7 @@ parse_opts(int argc, char **argv, struct options_t *opts)
 	opts->outflds = DFLT_OUTFLDS;
 
 	while ((ch = getopt(argc, argv,
-			    "H:uv" "B:D:E:F:IP:R:c:f:i:k:m:n:o:p:w:" "Vh"))
+			    "H:uv" "B:D:E:F:IP:R:b:c:f:i:k:m:n:o:p:w:" "Vh"))
 	       != -1)
 		switch (ch)
 		{
@@ -223,13 +223,17 @@ parse_opts(int argc, char **argv, struct options_t *opts)
 			opts->search_crit |= SEARCH_BY_RDEP;
 			opts->search_rdep = optarg;
 			break;
+		case 'b':
+			opts->search_crit |= SEARCH_BY_PFILE;
+			snprintf(opts->search_file, sizeof(opts->search_file), "/%s$", optarg);
+			break;
 		case 'c':
 			opts->search_crit |= SEARCH_BY_CAT;
 			opts->search_cat = optarg;
 			break;
 		case 'f':
 			opts->search_crit |= SEARCH_BY_PFILE;
-			opts->search_file = optarg;
+			snprintf(opts->search_file, sizeof(opts->search_file), "%s", optarg);
 			break;
 		case 'i':
 			opts->search_crit |= SEARCH_BY_INFO;
--- src/portsearch.h
+++ src/portsearch.h
@@ -27,23 +27,24 @@
 #ifndef PORTSEARCH_H
 #define PORTSEARCH_H
 
-#define PORTSEARCH_VERSION	"1.2.2"
+#include <sys/param.h>  /* for PATH_MAX */
 
-#define SEARCH_BY_PFILE			000001
-#define SEARCH_BY_NAME			000002
-#define SEARCH_BY_KEY			000004
-#define SEARCH_BY_PATH			000010
-#define SEARCH_BY_INFO			000020
-#define SEARCH_BY_MAINT			000040
-#define SEARCH_BY_CAT			000100
-#define SEARCH_BY_FDEP			000200
-#define SEARCH_BY_EDEP			000400
-#define SEARCH_BY_PDEP			001000
-#define SEARCH_BY_BDEP			002000
-#define SEARCH_BY_RDEP			004000
-#define SEARCH_BY_DEP			010000
-#define SEARCH_BY_WWW			020000
-#define SEARCH_BY_PFILE_BASENAME	040000
+#define PORTSEARCH_VERSION	"1.3.0"
+
+#define SEARCH_BY_PFILE		000001
+#define SEARCH_BY_NAME		000002
+#define SEARCH_BY_KEY		000004
+#define SEARCH_BY_PATH		000010
+#define SEARCH_BY_INFO		000020
+#define SEARCH_BY_MAINT		000040
+#define SEARCH_BY_CAT		000100
+#define SEARCH_BY_FDEP		000200
+#define SEARCH_BY_EDEP		000400
+#define SEARCH_BY_PDEP		001000
+#define SEARCH_BY_BDEP		002000
+#define SEARCH_BY_RDEP		004000
+#define SEARCH_BY_DEP		010000
+#define SEARCH_BY_WWW		020000
 
 #define DFLT_OUTFLDS	"name,path,info,maint,bdep,rdep,www"
 
@@ -52,7 +53,7 @@ struct options_t {
 	int		update_db;
 	int		verbose;
 	int		search_crit;
-	const char	*search_file;
+	char		search_file[PATH_MAX];
 	const char	*search_name;
 	const char	*search_key;
 	const char	*search_path;
